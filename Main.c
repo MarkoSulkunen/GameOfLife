c70b73a6af8680fe4af6d13ee81ebbdd0fc0e6b9
Marko Sulkunen <sulkunen.marko@gmail.com>
2023-02-21 19:48:28 +0200

    commit

--- Main.c
+++ Main.c
@@ -1,3 +1,25 @@
+/*********************************************************************
+
+ 1.  NAME
+       GameOfLife.c
+
+ 2.  DESCRIPTION
+       Game of life is the simplest evolution theory model.
+       Each cell with one or no neighbors' dies, as if by loneliness.
+       Each cell with four or more neighbors dies, as if by overpopulation.
+       Each cell with two or three neighbors survives.
+       Each cell with three neighbors becomes populated.
+
+
+
+ 3.  VERSIONS
+       Original:
+         14.5.2020 / Marko Sulkunen
+
+       Version history:
+
+**********************************************************************/
+
 #include "GOL.h"
 
 /*********************************************************************
